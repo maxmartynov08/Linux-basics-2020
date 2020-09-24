260444fa3671a8a47724839d50f4752102ee0dfd
Максим Мартынов <maksimmartynov@MacBook-Air.local>
2020-09-24 15:14:50 +0300

    Solve task02

--- seminar03/task02/task02.sh
+++ seminar03/task02/task02.sh
@@ -1,1 +1,13 @@
-#! /bin/bash
+#!/bin/bash
+a=$(($RANDOM * ($RANDOM / 1024)))
+div=2
+prime=true
+while [ $(($div * $div)) -le $a ];
+do
+    if [ $(($a % $div)) -eq 0 ];
+    then
+        prime=false
+    fi
+    div=$((div + 1))
+done
+echo "$a $prime"

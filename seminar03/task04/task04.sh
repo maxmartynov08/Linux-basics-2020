5e3d6766bc7af331d10db31460f67d035d6f7998
Максим Мартынов <maksimmartynov@MacBook-Air.local>
2020-09-30 19:56:40 +0300

    Solve task04

--- seminar03/task04/task04.sh
+++ seminar03/task04/task04.sh
@@ -1,3 +1,16 @@
 #! /bin/bash
-
-echo 2 24 120
+file="numbers.txt"
+while read line  || [ -n "$line" ];
+do
+    for x in $line;
+    do
+        fact=1
+        y=2
+        while [ $y -le $x ];
+        do
+            fact=$((fact * $y))
+            y=$((y + 1))
+        done
+        printf "$fact "
+    done
+done < "$file"

7a5c532b5f16ec2cecc4c7737cbd08a853eb85b8
Максим Мартынов <maksimmartynov@MacBook-Air.local>
2020-10-14 18:48:38 +0300

    Solve task03

--- seminar05/task3.sh
+++ seminar05/task3.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+commit_regex='issue-[\d]+.*)'
+error_msg="Aborting commit. Your commit message is missing issue"
+if ! grep -iqE "$commit_regex" "$1"
+then
+    echo "$error_msg" >&2
+    exit 1
+fi
+
